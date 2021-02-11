d0137ea3e43cdec23203dc7e55d4479d6ba86f56
Astudillo <cadomastu@gmail.com>
2021-02-11 19:41:33 +0100

    Componente producto creado

---
+++ frontend/src/app/components/body/principal/producto/producto.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-producto',
+  templateUrl: './producto.component.html',
+  styleUrls: ['./producto.component.css']
+})
+export class ProductoComponent implements OnInit {
+
+  constructor() { }
+  
+  public nombre: String = "Antena Parabolica"
+  public urlFoto: String
+  public descripcion: String
+  public precio: Number
+  public stock: Number
+
+  ngOnInit(): void {
+  }
+
+}
